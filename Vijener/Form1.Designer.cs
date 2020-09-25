64eaba93e67c353d26245f9043efaf9bf9a7beb0
drmix <drmix@DESKTOP-NO02ADD>
2020-09-25 16:19:45 +0500

    Шифр Цезаря

---
+++ Vijener/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Vijener
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.криптоанализToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opentextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opencrtextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.savetextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.savecrtextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveallToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.криптоанализToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(743, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "&Файл";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.exitToolStripMenuItem.Text = "&Выход";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // криптоанализToolStripMenuItem
+            // 
+            this.криптоанализToolStripMenuItem.Name = "криптоанализToolStripMenuItem";
+            this.криптоанализToolStripMenuItem.Size = new System.Drawing.Size(97, 20);
+            this.криптоанализToolStripMenuItem.Text = "&Криптоанализ";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(10, 98);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(334, 270);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox1_KeyPress);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(397, 99);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(334, 269);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Исходный текст:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(394, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Шифртекст:";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 382);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(743, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(142, 17);
+            this.toolStripStatusLabel1.Text = "Количество символов: 0";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(54, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(677, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ключ:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(350, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(41, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "▶";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(350, 237);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(41, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "◀";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.opentextToolStripMenuItem,
+            this.opencrtextToolStripMenuItem});
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "&Открыть";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.savetextToolStripMenuItem,
+            this.savecrtextToolStripMenuItem,
+            this.saveallToolStripMenuItem});
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "&Сохранить";
+            // 
+            // opentextToolStripMenuItem
+            // 
+            this.opentextToolStripMenuItem.Name = "opentextToolStripMenuItem";
+            this.opentextToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.opentextToolStripMenuItem.Text = "&Исходный текст";
+            // 
+            // opencrtextToolStripMenuItem
+            // 
+            this.opencrtextToolStripMenuItem.Name = "opencrtextToolStripMenuItem";
+            this.opencrtextToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.opencrtextToolStripMenuItem.Text = "&Шифртекст";
+            // 
+            // savetextToolStripMenuItem
+            // 
+            this.savetextToolStripMenuItem.Name = "savetextToolStripMenuItem";
+            this.savetextToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.savetextToolStripMenuItem.Text = "savetext";
+            // 
+            // savecrtextToolStripMenuItem
+            // 
+            this.savecrtextToolStripMenuItem.Name = "savecrtextToolStripMenuItem";
+            this.savecrtextToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.savecrtextToolStripMenuItem.Text = "savecrtext";
+            // 
+            // saveallToolStripMenuItem
+            // 
+            this.saveallToolStripMenuItem.Name = "saveallToolStripMenuItem";
+            this.saveallToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveallToolStripMenuItem.Text = "saveall";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 404);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Шифр Виженера";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem криптоанализToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opentextToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opencrtextToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem savetextToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem savecrtextToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveallToolStripMenuItem;
+    }
+}
+
---
+++ Vijener/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vijener
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Количество символов: " + ((RichTextBox)sender).Text.Length.ToString();
+        }
+
+        private int RusLetterToNumber(char c)
+        {
+            if (c == 'ё') return 7;
+            else
+            {
+                int tmp = (int)c - 1071;
+                if (tmp > 6) return tmp + 1;
+                else return tmp;
+            }
+        }
+
+        private char NumberToRusLetter(int n)
+        {
+            if (n == 0) return 'я';
+            if (n == 7) return 'ё';
+            else
+            {
+                if (n > 6) return (char)(n + 1070);
+                else return (char)(n + 1071);
+            }
+        }
+
+        private string Crypt(string text, string tkey)
+        {
+            Key key = new Key(tkey.ToLower());
+            string result = "";
+            int code;
+            foreach (char c in text.ToLower())
+            {
+                if (c != ' ')
+                {
+                    code = (RusLetterToNumber(c) + RusLetterToNumber(key.Next())) % 33;
+                    result += NumberToRusLetter(code);
+                }
+                else result += c;
+            }
+            return result;
+        }
+
+        private string Decrypt(string crtext, string tkey)
+        {
+            Key key = new Key(tkey.ToLower());
+            string result = "";
+            int code;
+            foreach (char c in crtext.ToLower())
+            {
+                if (c != ' ')
+                {
+                    code = (RusLetterToNumber(c) + 33 - RusLetterToNumber(key.Next())) % 33;
+                    result += NumberToRusLetter(code);
+                }
+                else result += c;
+            }
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text.Length == 0) { MessageBox.Show("Введите исходный текст!"); return; }
+            if (textBox1.Text.Length == 0) { MessageBox.Show("Введите ключ!"); return; }
+            richTextBox2.Text = Crypt(richTextBox1.Text, textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (richTextBox2.Text.Length == 0) { MessageBox.Show("Введите шифртекст!"); return; }
+            if (textBox1.Text.Length == 0) { MessageBox.Show("Введите ключ!"); return; }
+            richTextBox1.Text = Decrypt(richTextBox2.Text, textBox1.Text);
+        }
+
+        private void richTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < 'а' || e.KeyChar > 'я') && e.KeyChar != ' ') e.Handled = true;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+
+    public class Key
+    {
+        int currentindex;
+        string keystr;
+        public Key(string key)
+        {
+            keystr = key; currentindex = 0;
+        }
+        public char Next()
+        {
+            char res = keystr[currentindex];
+            if (currentindex != keystr.Length - 1) currentindex++;
+            else currentindex = 0;
+            return res;
+        }
+    }
+}
